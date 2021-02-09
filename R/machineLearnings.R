e0008a4baa2eed2fe1d9bbb6c2cade8c962c4e5c
zhamadeh <zeid.hamadeh@hotmail.com>
2021-02-09 09:55:46 -0800

    new ploots, changes

--- R/machineLearnings.R
+++ R/machineLearnings.R
@@ -63,9 +63,9 @@ for (colname in colnames(metrics)){
 		metrics$Sequencing_for_0.05x_cov <- as.numeric(metrics$Sequencing_for_0.05x_cov )
 		metrics$Sequencing_for_0.05x_cov[is.na(metrics$Sequencing_for_0.05x_cov)] <- 2 #replace_na(metrics$Sequencing_for_0.05x_cov) <- 2
 	}
-	if (colname == "Percent_WC"|colname == "complexity" ){
-		metrics[,colname][is.na(metrics[,colname])] <- 0
-	}
+	#if (colname == "Percent_WC"|colname == "complexity" ){
+	#	metrics[,colname][is.na(metrics[,colname])] <- 0
+	#}
 }
 #annotate empty libraries
 for (row in 1:nrow(metrics)){
@@ -83,23 +83,23 @@ write.table(metrics,"Output/Metrics/completeMetrics.txt",sep="\t",row.names = F,
 write.table(metrics$Library,"Output/TraningLibraries/trainingLibraries.txt",sep="\t",row.names = F,col.names = F,quote=F)
 #remove NA rows and library feature
 #metrics=na.exclude(metrics)
-metrics=select(metrics,-c(Library))
+metrics=select(metrics,-c(Library,Naive_coverage ,Sequencing_for_0.05x_cov))
 
 
 for (row in 1:nrow(metrics)){
 	if (metrics[row,]$Quality=="acceptable" | metrics[row,]$Quality=="a" ){
-		metrics[row,]$Quality<- "good"
+		metrics[row,]$Quality<- "god"
 	} else if (metrics[row,]$Quality=="brdu" | metrics[row,]$Quality=="b" ){
-		metrics[row,]$Quality="brdu"
+		metrics[row,]$Quality="por"
 	} else if (metrics[row,]$Quality=="p"){
 		metrics[row,]$Quality="poor"
-	}#else if (metrics[row,]$Quality=="shallow"){
-		#metrics[row,]$Quality="poor"
-	#}
+	}else if (metrics[row,]$Quality=="shallow"){
+		metrics[row,]$Quality="poor"
+	}
 }
 
 metrics$Quality <- droplevels.factor(metrics$Quality)
-metrics %>% group_by(Quality)%>% summarize(n())
+metrics %>% group_by(Quality)%>% summarize((n()/2638)*100)
 
 
 ################## Data partitioning ##################
@@ -131,23 +131,63 @@ trainData <- metrics[validation_index,] # use the remaining 80% of data to train
 ################### Preliminary plotting ##################
 #just  to see if any variables are obviously important
 
-x <- trainData[,c("Reads","complexity","Background","spikiness","entropy","coverage")] #metrics
-y <- trainData[,5] #classification
-scales <- list(x=list(relation="free"), y=list(relation="free"))
+#x <- trainData[,c("Reads","complexity","Background","spikiness","entropy","coverage")] #metrics
+#y <- trainData[,5] #classification
+#scales <- list(x=list(relation="free"), y=list(relation="free"))
 #featurePlot(x=x, y=y, plot="box",scales =scales)
 #featurePlot(x=x, y=y, plot="density",scales =scales,strip=strip.custom(par.strip.text=list(cex=.7)))
 #featurePlot(x = metrics[, c("Background","Reads","complexity","spikiness","entropy")],y = metrics$Quality,plot = "pairs",auto.key = list())
 #pairs(metrics[, c("complexity","spikiness","entropy","Background","Reads")],col=metrics$Quality)
-
-
 
 ################## Training models ##################
 
 control <- trainControl(method="cv", number=10,classProbs = T)
 metric <- "Accuracy"
 
+library(DMwR)
+
+
+set.seed(9560)
+smote_train <- SMOTE(Quality ~ ., data  = trainData)
+smote_outside <- train(Quality ~ ., data = smote_train,
+					   method = "treebag",
+					   nbagg = 50,
+					   metric = "ROC",
+					   trControl = control)
+library(ROSE)
+
+set.seed(9560)
+rose_train <- ROSE(Quality ~ ., data  = trainData)$data
+set.seed(5627)
+rose_outside <- train(Quality ~ ., data = rose_train,
+					  method = "treebag",
+					  nbagg = 50,
+					  metric = "ROC",
+					  trControl = control)
+outside_models <- list(SMOTE = smote_outside,ROSE=rose_outside)
+
+outside_resampling <- resamples(outside_models)
+
+test_roc <- function(model, data) {
+	library(pROC)
+	roc_obj <- roc(data$Quality,
+				   predict(model, data, type = "prob")[, "good"],
+				   levels = c("good", "poor"))
+	ci(roc_obj)
+}
+
+outside_test <- lapply(outside_models, test_roc, data = trainData)
+outside_test <- lapply(outside_test, as.vector)
+outside_test <- do.call("rbind", outside_test)
+colnames(outside_test) <- c("lower", "ROC", "upper")
+outside_test <- as.data.frame(outside_test)
+
+summary(outside_resampling)
+
+
 #Linear (lda), non-linear(CART,kNN), advanced
 models=c("lda","rpart","knn","svmRadial","rf","gbm")
+#model=models[1]
 for (model in models){
 	set.seed(7)
 	fit <- train(Quality~., data=trainData, method=model, metric=metric, trControl=control)
@@ -160,16 +200,18 @@ for (model in models){
 #fit.gbm =  train(Quality~., data=trainData, method="gbm", metric=metric, trControl=control)
 
 results <- resamples(list(lda=fit.lda,rpart=fit.rpart,knn=fit.knn,svmRadial=fit.svmRadial,rf=fit.rf,gbm=fit.gbm))
-summary(results)
+#summary(results)
 dotplot(results)
-fit.rf
 #default_knn_mod = train(Quality ~ .,data = trainData,method = "knn",trControl = trainControl(method = "cv", number = 5),preProcess = c("center", "scale"),tuneGrid = expand.grid(k = seq(1, 101, by = 2)))
 #fit.gbm.imp <- varImp(fit.gbm)
 #plot(fit.rf.imp, main="Variable Importance with Random Forest")
-plot(fit.gbm, main="Model Accuracies with Random Forest")
+#plot(fit.gbm, main="Model Accuracies with Random Forest")
 
 #Running best model on testData/test set
-predictions <- predict(fit.gbm, na.omit(testData))
+predictions <- predict(fit.rf, na.omit(testData))
+confusionMatrix(predictions, testData$Quality)
+
+predictions <- predict(rose_outside, na.omit(testData))
 confusionMatrix(predictions, testData$Quality)
 
 
